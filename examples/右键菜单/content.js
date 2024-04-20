cfbc01e99f154115f3010fc4fa854b363527a65a
lovefc <fcphp@qq.com>
2024-04-20 15:40:59 +0800

    test

--- examples/右键菜单/content.js
+++
@@ -1,5 +0,0 @@
-document.body.style.backgroundColor = "#000";
-document.body.style.backgroundImage = 'url(https://cdn.pixabay.com/photo/2022/12/01/04/35/sunset-7628294_1280.jpg)';
-document.body.style.backgroundSize = 'cover';
-document.body.style.backgroundPosition = 'center';
-document.body.style.backgroundRepeat = 'no-repeat';
