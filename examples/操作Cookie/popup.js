ff810ec589d984e29d49bdc69c0a63ec5d12d466
lovefc <fcphp@qq.com>
2024-05-08 14:16:34 +0800

    test

--- examples/操作Cookie/popup.js
+++ examples/操作Cookie/popup.js
@@ -20,6 +20,22 @@ let cookie_array = [
   }
 ];
 
+function login() {
+  const loginData = {
+    username: '20240508',
+    password: '5201314'
+  };
+  // 登录URL
+  const loginUrl = 'https://www.asklib.com/user.php?act=login';
+  // 发送POST请求进行登录
+  axios.post(loginUrl, loginData).then(response => {
+    console.log('登录成功', response);
+  }).catch(error => {
+    // 登录失败处理逻辑
+    console.error('登录失败', error);
+  });
+
+}
 
 // 设置cookie
 async function set(cookie) {
@@ -62,14 +78,21 @@ const button = document.getElementById('get');
 button.addEventListener('click', async function (event) {
   let cookies = await get('PHPSESSID', domain);
   console.log(cookies);
-  document.getElementById('cookie_text').innerHTML = cookies.value;
+  if (cookies.value) {
+    document.getElementById('cookie_text').innerHTML = cookies.value;
+  }
 });
 
 // 设置
 const button2 = document.getElementById('set');
 button2.addEventListener('click', async function (event) {
+  // 登录一下，因为这个是session，不登陆一下，设置了也没用(服务器那边会删除)。
+  // 注意，这里登录后其实已经有cookies了
+  login();
   for (let key in cookie_array) {
+    // 删掉原来的
     await remove(cookie_array[key].name, cookie_array[key].url);
+    // 设置新的
     await set(cookie_array[key]);
   }
   alert('cookie设置成功');
@@ -78,6 +101,7 @@ button2.addEventListener('click', async function (event) {
 
 // 删除
 const button3 = document.getElementById('remove');
+
 button3.addEventListener('click', async function (event) {
   for (let key in cookie_array) {
     await remove(cookie_array[key].name, cookie_array[key].url);
