ab429284b0ae374f8272ef394c5e1867d5bc4418
lovefc <fcphp@qq.com>
2024-05-16 14:02:36 +0800

    test

---
+++ examples/知乎消息/background.js
@@ -0,0 +1,37 @@
+// chrome.action文档：https://developer.chrome.com/docs/extensions/reference/api/action?hl=zh-cn
+// 请注意参考
+
+let zhihu_count = 0; // 知乎消息
+
+function setMsg(str){
+	chrome.action.setBadgeText({text: str});
+	chrome.action.setBadgeBackgroundColor({color:'#B22222'});	
+}
+
+// 获取知乎消息
+async function getZhihuMsgCount() {
+    const response = await fetch("https://www.zhihu.com/api/v4/me");
+    const jsonData = await response.json();	
+	if(jsonData.error){
+		console.log('未登录知乎');
+		return;
+	}
+	// 读取所有的消息
+	let messages_count = jsonData.messages_count;
+	let default_notifications_count = jsonData.default_notifications_count;
+	let vote_thank_notifications_count = jsonData.vote_thank_notifications_count;
+	let follow_notifications_count = jsonData.follow_notifications_count;
+	let number = messages_count + default_notifications_count + vote_thank_notifications_count + follow_notifications_count;	
+	
+	// 设置图标和消息
+	if(number && number!=0){
+		chrome.action.setIcon({path:"images/zhihu.png"});
+		setMsg(number.toPrecision());
+	}	
+};
+
+// 定时抓取
+setInterval(function(){
+	getZhihuMsgCount();
+},6666);
+
