9be9cbeb481f0e62fe24bfb126e2b739d9b7473d
lovefc <fcphp@qq.com>
2024-05-18 15:37:48 +0800

    test

--- examples/知乎消息/background.js
+++ examples/知乎消息/background.js
@@ -3,16 +3,22 @@
 
 let zhihu_count = 0; // 知乎消息
 
-function setMsg(str){
-	chrome.action.setBadgeText({text: str});
-	chrome.action.setBadgeBackgroundColor({color:'#B22222'});	
+function setMsg(str) {
+	chrome.action.setBadgeText({ text: str });
+	chrome.action.setBadgeBackgroundColor({ color: '#B22222' });
 }
 
 // 获取知乎消息
 async function getZhihuMsgCount() {
-    const response = await fetch("https://www.zhihu.com/api/v4/me");
-    const jsonData = await response.json();	
-	if(jsonData.error){
+	let jsonData = { error: 'not' };
+	try {
+		const response = await fetch("https://www.zhihu.com/api/v4/me");
+		jsonData = await response.json();
+	} catch (error) {
+		console.log(error);
+		return;
+	}
+	if (jsonData.error) {
 		console.log('未登录知乎');
 		return;
 	}
@@ -21,17 +27,17 @@ async function getZhihuMsgCount() {
 	let default_notifications_count = jsonData.default_notifications_count;
 	let vote_thank_notifications_count = jsonData.vote_thank_notifications_count;
 	let follow_notifications_count = jsonData.follow_notifications_count;
-	let number = messages_count + default_notifications_count + vote_thank_notifications_count + follow_notifications_count;	
-	
+	let number = messages_count + default_notifications_count + vote_thank_notifications_count + follow_notifications_count;
+
 	// 设置图标和消息
-	if(number && number!=0){
-		chrome.action.setIcon({path:"images/zhihu.png"});
+	if (number && number != 0) {
+		chrome.action.setIcon({ path: "images/zhihu.png" });
 		setMsg(number.toPrecision());
-	}	
+	}
 };
 
 // 定时抓取
-setInterval(function(){
+setInterval(function () {
 	getZhihuMsgCount();
-},6666);
+}, 6666);
 
